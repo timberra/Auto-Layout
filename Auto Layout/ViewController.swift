792447f536576584ba104ffb3455663a7967672a
timberra <145005326+timberra@users.noreply.github.com>
2023-10-20 15:00:38 +0300

    Initial Commit

---
+++ Auto Layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Auto Layout
+//
+//  Created by liga.griezne on 20/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
